18213e574cdb9cd99d4beeff21beaeb46ade007f
Yassin-Aru <jocarhisouka@gmail.com>
2024-02-10 22:25:52 +0100

    feat: add IncomeExpenses component

---
+++ src/components/IncomeExpenses.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex justify-between items-center w-full mt-8 bg-black rounded-lg p-4">
+    <div class="w-1/2 text-center">
+      <h4 class="text-lg uppercase font-bold">Income</h4>
+      <p id="money-plus" class="text-3xl mt-3 font-extrabold">+0$</p>
+    </div>
+    <div class="w-1/2  text-center">
+      <h4 class="text-lg uppercase font-bold">Expense</h4>
+      <p id="money-minus" class="text-3xl mt-3 font-extrabold">-0$</p>
+    </div>
+  </div>
+</template>
