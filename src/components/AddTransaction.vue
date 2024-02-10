2a56e4de7a70533a49fa793458ceab385a15da92
Yassin-Aru <jocarhisouka@gmail.com>
2024-02-10 22:24:18 +0100

    feat: add AddTransaction component

---
+++ src/components/AddTransaction.vue
@@ -0,0 +1,39 @@
+<template>
+  <form class="">
+    <h4 class="text-xl font-bold">Add Transaction</h4>
+    <div class="space-y-2 w-full mt-4">
+      <label
+        class="text-lg font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+        for="text"
+      >
+        Text
+      </label>
+      <input
+        class="flex h-10 w-full rounded-md px-3 py-3 text-base ring-offset-background bg-grey-200"
+        id="text"
+        placeholder="Enter your text"
+      />
+    </div>
+    <div class="space-y-2 w-full mt-4">
+      <label
+        class="text-lg font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+        for="amount"
+      >
+        Amount
+        <br />
+        <span class="text-base">(positive - income, negative - expense)</span>
+      </label>
+      <input
+        class="flex h-10 w-full rounded-md px-3 py-3 text-base ring-offset-background bg-grey-200"
+        id="amount"
+        placeholder="Enter your amount"
+      />
+    </div>
+    <button
+      type="submit"
+      class="mt-4 mb-4 bg-white w-full p-2 text-black font-bold text-lg rounded"
+    >
+      Submit
+    </button>
+  </form>
+</template>
