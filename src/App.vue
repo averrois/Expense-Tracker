2da0ca030294faebe3651d00a6384ca98e1ac870
Yassin-Aru <jocarhisouka@gmail.com>
2024-02-10 22:26:24 +0100

    feat: update App

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,19 @@
-<template> 
-  <h1 class="text-3xl font-bold underline text-primary">Yellow World</h1>
+<script setup lang="ts">
+import Header from "@/components/Header.vue";
+import Balance from "@/components/Balance.vue";
+import IncomeExpenses from "@/components/IncomeExpenses.vue";
+import TransactionList from "@/components/TransactionList.vue";
+import AddTransaction from "./components/AddTransaction.vue";
+</script>
+
+<template>
+  <main
+    class="border-[2px] text-left max-sm:w-full border-grey-200 bg-grey-100 px-6 py-2 rounded mx-auto w-[500px]"
+  >
+    <Header />
+    <Balance />
+    <IncomeExpenses />
+    <TransactionList />
+    <AddTransaction />
+  </main>
 </template>
