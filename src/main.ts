a15714ff14d8574e159dc05d8f52fb46a094c2e1
Yassin-Aru <jocarhisouka@gmail.com>
2024-02-11 12:13:51 +0100

    deps: install vue-toastification

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import './assets/main.css'
-
+import Toast from 'vue-toastification'
+import 'vue-toastification/dist/index.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App).use(Toast).mount('#app')
